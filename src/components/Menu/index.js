f93639452ea244aadad6b853a8d1f1de828b50ff
AdiMondal <144584127+AdiMondal@users.noreply.github.com>
2024-01-22 09:12:58 +0530

    added communication between toolbox and menu using redux toolkit

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,25 @@
+import {  useDispatch, useSelector} from 'react-redux'
+import cx from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEraser, faFileArrowDown, faPencil, faRotateLeft, faRotateRight } from '@fortawesome/free-solid-svg-icons'
 import styles from './index.module.css'
+import { MENU_ITEMS } from '@/src/constants'
+import { menuItemClick, actionItemClick } from '@/src/slice/menuSlice'
 
 const Menu = ()=>{
+    const dispatch = useDispatch()
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const handleMenuClick = (itemName) =>{
+      dispatch(menuItemClick(itemName))
+    }
+
     return (
        <div className={styles.menuContainer}>
-           <div className={styles.iconWrapper}>
-             <FontAwesomeIcon icon={faPencil} className={styles.icon}/>
+           <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.PENCIL})} onClick={() => handleMenuClick(MENU_ITEMS.PENCIL)}>
+             <FontAwesomeIcon icon={faPencil} className={styles.icon} />
            </div>
-            <div className={styles.iconWrapper} >
-             <FontAwesomeIcon icon={faEraser} className={styles.icon}/>
+            <div className={cx(styles.iconWrapper, {[styles.active]: activeMenuItem === MENU_ITEMS.ERASER})} onClick={() => handleMenuClick(MENU_ITEMS.ERASER)}>
+             <FontAwesomeIcon icon={faEraser} className={styles.icon}  />
            </div>
             <div className={styles.iconWrapper}>
              <FontAwesomeIcon icon={faRotateLeft} className={styles.icon} />
