518d4e2f02f8b420ced0c7da1582ab10ce5577ca
AdiMondal <144584127+AdiMondal@users.noreply.github.com>
2024-01-22 07:13:58 +0530

    styles done

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Menu from "@/src/components/Menu"
 import Toolbox from "@/src/components/Toolbox"
+import Board from "@/src/components/Board"
 
 export default function Home() {
   return (
     <> 
      <Menu />
      <Toolbox />
+     <Board />
     </>
    
   )
---
+++ src/components/Board/index.js
@@ -0,0 +1,10 @@
+import { useRef } from "react";
+
+const Board = () => {
+    const canvasRef = useRef(null)
+    return(
+          <canvas ref = {canvasRef} ></canvas>
+    )
+}
+
+export default Board;
