3a2ddcc07701f1c4a7d51d8811d2b682ed276381
AdiMondal <144584127+AdiMondal@users.noreply.github.com>
2024-01-25 16:06:14 +0530

    i can draw now yay =D

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -3,8 +3,23 @@ import { UseSelector, useDispatch, useSelector } from "react-redux";
 
 const Board = () => {
     const canvasRef = useRef(null)
+    const shouldDraw = useRef(false)
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
     const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+    
+    useEffect(()=>{
+       if(!canvasRef.current)return
+       const canvas = canvasRef.current;
+       const context = canvas.getContext('2d')
+       
+       const changeConfig=() =>{
+        context.strokeStyle =color
+        context.lineWidth = size
+       }
+        
+      changeConfig()
+    },[color, size])
+
     useEffect(() => {
        if(!canvasRef.current)return
        const canvas = canvasRef.current;
@@ -13,6 +28,30 @@ const Board = () => {
        //when mounting
        canvas.width = window.innerWidth
        canvas.height = window.innerHeight
+
+       const handleMouseDown = (e) =>{
+          shouldDraw.current = true
+          context.beginPath()
+          context.moveTo(e.clientX, e.clientY)
+       }
+       const handleMouseMove = (e) =>{
+         if(!shouldDraw.current)return
+         context.lineTo(e.clientX, e.clientY)
+         context.stroke()
+       }
+       const handleMouseUp = (e) =>{
+        shouldDraw.current = false
+       }
+
+       canvas.addEventListener('mousedown', handleMouseDown)
+       canvas.addEventListener('mousemove', handleMouseMove)
+       canvas.addEventListener('mouseup', handleMouseUp)
+      
+       return() => {
+          canvas.removeEventListener('mousedown', handleMouseDown)
+         canvas.removeEventListener('mousemove', handleMouseMove)
+         canvas.removeEventListener('mouseup', handleMouseUp)
+        }
     } , [])   
     console.log(color,size)
     return(
@@ -21,4 +60,5 @@ const Board = () => {
 }
 
 export default Board;
+
 
